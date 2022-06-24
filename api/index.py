2dc063dfe902fc720954a00389f45180e0e9213d
rr210 <1577791638@qq.com>
2022-06-24 20:30:54 +0800

    fix:更新依赖

--- api/index.py
+++ api/index.py
@@ -5,10 +5,10 @@
 # @Date  : 24/6/2022
 # @Desc  :
 import json
-import uuid
 from flask import Flask, render_template, request
 import os
-from python import B2
+import uuid
+from b2sdk.v2 import *
 
 app = Flask(__name__, template_folder='static', static_url_path='', static_folder='static')
 
@@ -42,7 +42,29 @@ def upload():
             return t
         except:
             return json.dumps({"msg": "上传失败", "status_code": -1})
-#
-#
-# if __name__ == "__main__":
-#     app.run('127.0.0.1', port=501)
+
+class B2:
+    def __init__(self, application_key_id, application_key, bucket_name, host_url):
+        self.application_key_id = application_key_id
+        self.application_key = application_key
+        self.bucket_name = bucket_name
+        self.host_url = host_url
+
+    def main(self):
+        info = InMemoryAccountInfo()
+        b2_api = B2Api(info)
+        b2_api.authorize_account("production", self.application_key_id, self.application_key)
+        b2_api.list_buckets()
+        return b2_api
+
+    def uploadLocalImg(self, local_file_path, to_file, format_):
+        b2_file_name = to_file + str(uuid.uuid4()) + '.' + format_
+        bucket = self.main().get_bucket_by_name(self.bucket_name)
+        t = bucket.upload_local_file(
+            local_file=local_file_path,
+            file_name=b2_file_name
+        )
+        print(t)
+        print('图片地址\n' + self.host_url + b2_file_name)
+        return {"msg": "上传成功", "status_code": 1, 'link': self.host_url + b2_file_name}
+
