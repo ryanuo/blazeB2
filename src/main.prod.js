056f46778fc713c6e97fc031c3ff9ca9115b76c6
rr210 <157791638@qq.com>
2022-07-12 16:57:33 +0800

    ✨feat: 支持pwa;

--- src/main.prod.js
+++ src/main.prod.js
@@ -3,8 +3,8 @@
  * @Date: 2022-04-20 20:40:43
  * @LastEditors: harry
  * @Github: https://github.com/rr210
- * @LastEditTime: 2022-07-09 22:30:31
- * @FilePath: \master\src\main.prod.js
+ * @LastEditTime: 2022-07-12 16:53:16
+ * @FilePath: \dev\src\main.prod.js
  */
 
 /* eslint-disable */
@@ -15,6 +15,7 @@ import App from './App.vue'
 import router from './router'
 import ELEMENT from 'element-ui'
 import VueClipboard from 'vue-clipboard2'
+import './registerServiceWorker'
 // import element from './plugin/elements.js'
 import './assets/css/global.less'
 import './assets/css/element-variables.scss'
